c665739875bfaf2333abf9126f7f3ae4956623d4
alekseinovikov <novikov.aleksei.92@gmail.com>
2026-04-06 17:51:01 +0300

    Updated logo and title

--- composeApp/src/jvmMain/kotlin/me/alekseinovikov/open_gtd/main.kt
+++ composeApp/src/jvmMain/kotlin/me/alekseinovikov/open_gtd/main.kt
@@ -2,11 +2,15 @@ package me.alekseinovikov.open_gtd
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import open_gtd.composeapp.generated.resources.Res
+import open_gtd.composeapp.generated.resources.icon
+import org.jetbrains.compose.resources.painterResource
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        title = "open_gtd",
+        title = "Open GTD",
+        icon = painterResource(Res.drawable.icon),
     ) {
         App()
     }
